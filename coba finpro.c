033165d938fa45d992d6c919a8d5a7e61f1f74a3
aliciakyoumi <aliciabastian99@gmail.com>
2025-05-23 17:07:00 +0700

    tambah (cia, qila, belva)

--- coba finpro.c
+++ coba finpro.c
@@ -35,13 +35,11 @@ int scoreBMI(float bmi, const char* gender) {
     return -1;
 }
 
-
 int scoreGlucose(float glucose) {
     if (glucose < 7.8) return 0;
     else if (glucose < 11.1) return 1;
     else return 2;
 }
-
 
 int scoreBP(float systolic, float diastolic) {
     if (systolic < 130 && diastolic < 80) return 0;
@@ -49,19 +47,16 @@ int scoreBP(float systolic, float diastolic) {
     else return 2;
 }
 
-
 int scoreChol(float cholesterol) {
     if (cholesterol < 5.0) return 0;
     else if (cholesterol < 7.5) return 1;
     else return 2;
 }
 
-
 int scoreSmoker(const char* status) {
     if (strcmp(status, "tidak") == 0) return 0;
     else return 2;
 }
-
 
 const char* overallRiskLevel(int totalScore) {
     if (totalScore <= 2) return "Sehat";
@@ -69,18 +64,14 @@ const char* overallRiskLevel(int totalScore) {
     else return "Risiko Tinggi";
 }
 
-
 void evaluateAndSave(struct Patient* p, FILE* file) {
     float bmi = calculateBMI(p->weight, p->height);
-
-
     int score = 0;
     score += scoreBMI(bmi, p->gender);
     score += scoreGlucose(p->glucose);
     score += scoreBP(p->systolic, p->diastolic);
     score += scoreChol(p->cholesterol);
     score += scoreSmoker(p->smokerStatus);
-
 
     fprintf(file, "\n=== Laporan Evaluasi Pasien %s (%s) ===\n", p->name, p->id);
     fprintf(file, "Usia: %d | Jenis Kelamin: %s\n", p->age, p->gender);
@@ -97,10 +88,8 @@ int main() {
     struct Patient patients[MAX];
     int n, i;
 
-
     printf("Masukkan jumlah pasien: ");
     scanf("%d", &n);
-
 
     for (i = 0; i < n; i++) {
         printf("\n--- Input Data Pasien ke-%d ---\n", i + 1);
@@ -132,22 +121,18 @@ int main() {
         scanf("%s", patients[i].smokerStatus);
     }
 
-
     FILE* file = fopen("laporan_pasien.txt", "w");
     if (!file) {
         printf("Gagal membuka file output!\n");
         return 1;
     }
 
-
     for (i = 0; i < n; i++) {
         evaluateAndSave(&patients[i], file);
     }
 
-
     fclose(file);
     printf("\n Evaluasi selesai. Laporan tersimpan di 'laporan_pasien.txt'\n");
-
 
     return 0;
 }
